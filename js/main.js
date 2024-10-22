650c1593739b9d0bcce035111ea478c8af4a948c
Vitor Emanoel da Silva Nogueira <vitoremanoelnogueira@gmail.com>
2024-10-22 09:08:28 -0300

    added share section functionality and styles
    added share section functionality and styles and fixed some styles in other elements

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const button = document.querySelector('.button--share');
+
+button.addEventListener('click', showShare);
+button.addEventListener('touch', showShare);
+
+function showShare() {
+    const shareSection = document.querySelector('.share-section')
+    const iconDesign = document.querySelector('.icon__design');
+
+    shareSection.style.display = (shareSection.style.display === "none" || shareSection.style.display === "") ? "flex" : "none";
+
+    iconDesign.style.fill = (iconDesign.style.fill === "white") ? "#6E8098" : "white";
+
+
+    if(button.classList.contains('bg--desatured-dark-blue') === true){
+        button.classList.remove('bg--desatured-dark-blue');
+        button.classList.toggle('bg--light-grayish-blue')
+        button.blur();
+    }else{
+        button.classList.remove('bg--light-grayish-blue');
+        button.classList.toggle('bg--desatured-dark-blue');
+    }
+}
+
+
